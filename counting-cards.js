86f3cac5e3d95e7d363140596d0fdcd9abb51654
samrodriver <43504379+samrodriver@users.noreply.github.com>
2019-10-29 18:18:52 +0100

    Add files via upload

---
+++ counting-cards.js
@@ -0,0 +1,31 @@
+var count = 0;
+
+function cc(card) {
+
+  switch(card)
+  {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6: count +=1;
+    break;
+    case 7:
+    case 8:
+    case 9: count +=0;
+    break;
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A': count-=1;
+  }
+
+  if(count <= 0)
+  {
+  return count + " Hold";
+  }else
+  {
+    return count + " Bet";
+  }
+}
